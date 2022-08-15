41c9c924e1f1bd00d0dd19bf95646566c8e7b79d
viniciusddrft <viniciusddrft5@gmail.com>
2022-08-15 16:20:24 -0300

    update code

--- app_van/lib/src/modules/home/home_page.dart
+++ app_van/lib/src/modules/home/home_page.dart
@@ -8,8 +8,89 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  late final Size size;
+
+  @override
+  void didChangeDependencies() {
+    size = MediaQuery.of(context).size;
+    super.didChangeDependencies();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.purple.shade700,
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        title: const Text('Van'),
+        actions: const [
+          Padding(
+            padding: EdgeInsets.only(right: 10),
+            child: Icon(Icons.person),
+          )
+        ],
+      ),
+      body: FractionallySizedBox(
+        widthFactor: 1,
+        child: Column(
+          children: [
+            RichText(
+              textAlign: TextAlign.center,
+              text: const TextSpan(
+                text: 'Olá,\n',
+                style: TextStyle(fontSize: 30),
+                children: [
+                  TextSpan(
+                    text: 'Bellato',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 15, bottom: 15),
+              child: SizedBox(
+                height: size.height * 0.6,
+                width: size.width * 0.8,
+                child: Card(
+                  color: Colors.blue,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: const [
+                        Text(
+                          'Alunos:',
+                          style: TextStyle(fontSize: 30, color: Colors.white),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 150),
+                          child: Text('CALMA QUE O RESTO EU JÁ FAÇO',
+                              style:
+                                  TextStyle(fontSize: 30, color: Colors.white)),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: const SizedBox(
+                height: 80,
+                width: 200,
+                child: Center(
+                  child: Text(
+                    'Iniciar corrida',
+                    style: TextStyle(fontSize: 30),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
